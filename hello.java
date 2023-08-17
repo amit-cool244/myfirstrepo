d110207ebda32024cc20cb3694cfd25e77f0d79a
amit <amit.cool244@gmail.com>
2023-08-17 01:53:11 +0000

    master comment added

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Change happened");
+        System.out.println("Helo from master");
         System.out.println("Hello from child!");    
 }
 }
